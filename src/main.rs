e42d34509fccf7f256bd0ed0082fd4753d21de28
Martin Slot <slot@sigsys.dk>
2020-10-22 16:19:36 +0200

    Refactored commit and stage out in some functions

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,36 @@
 use git2::Commit;
+use git2::Index;
 use git2::ObjectType;
 use git2::Repository;
 use std::env;
 use std::fs::File;
 use std::path::Path;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let root = &args[1];
+    let file_name = &args[2];
+    let repo = if Path::new(root).exists() {
+        println!("open");
+        Repository::open(root).unwrap()
+    } else {
+        println!("init");
+        Repository::init(root).unwrap()
+    };
+
+    let mut index = stage(&repo, root, file_name);
+    commit(&repo, &mut index);
+}
+fn stage(repo: &Repository, root: &str, file_name: &str) -> Index {
+    let mut index = repo.index().unwrap();
+
+    File::create(Path::new(root).join(file_name)).unwrap();
+
+    index.add_path(Path::new(file_name)).unwrap();
+    index.write().unwrap();
+
+    return index;
+}
 
 fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
     let obj = repo.head()?.resolve()?.peel(ObjectType::Commit)?;
@@ -11,17 +38,7 @@ fn find_last_commit(repo: &Repository) -> Result<Commit, git2::Error> {
         .map_err(|_| git2::Error::from_str("Couldn't find commit"))
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let root = &args[1];
-    let file_name = &args[2];
-    let repo = Repository::init(root).unwrap();
-    let mut index = repo.index().unwrap();
-
-    File::create(Path::new(root).join(file_name)).unwrap();
-
-    index.add_path(Path::new(file_name)).unwrap();
-    index.write().unwrap();
+fn commit(repo: &Repository, index: &mut git2::Index) {
     let lc_result = find_last_commit(&repo);
 
     let mut parent_commits: Vec<&Commit> = vec![];
@@ -31,7 +48,9 @@ fn main() {
         Err(_) => (),
     };
 
+    index.write().unwrap();
     let id = index.write_tree().unwrap();
+    index.write().unwrap();
     let tree = repo.find_tree(id).unwrap();
 
     repo.commit(
